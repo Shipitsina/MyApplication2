f71dcb4070e29daedb0af2b397492b4ca1252e6e
Shipitsina <kybeg@inbox.ru>
2021-08-03 21:35:15 +0500

    попытка слияния1

--- app/src/main/java/ru/gb/shipitsina/myapplication2/MainActivity.java
+++ app/src/main/java/ru/gb/shipitsina/myapplication2/MainActivity.java
@@ -9,15 +9,24 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-import ru.gb.shipitsina.myapplication2.ui.NotesFragment;
+import ru.gb.shipitsina.myapplication2.data.CardsSource;
+import ru.gb.shipitsina.myapplication2.data.CardsSourceImpl;
+import ru.gb.shipitsina.myapplication2.ui.ChosenNoteFragment;
+import ru.gb.shipitsina.myapplication2.ui.NotesAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -120,5 +129,69 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+    public static class NotesFragment extends Fragment {
+
+        public static final String CURRENT_NOTE = "CurrentNote";
+        private Note currentNote;
+        private boolean isLandscape;
+
+        public static NotesFragment newInstance() {
+            return new NotesFragment();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.fragment_notes, container, false);
+            RecyclerView recyclerView = view.findViewById(R.id.recycler_view_lines);
+            // Получим источник данных для списка
+            CardsSource data = new CardsSourceImpl(getResources()).init();
+            initRecyclerView(recyclerView, data);
+            return view;
+        }
+
+        private void initRecyclerView(RecyclerView recyclerView, CardsSource data){
+
+            // Эта установка служит для повышения производительности системы
+            recyclerView.setHasFixedSize(true);
+
+            // Будем работать со встроенным менеджером
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+            recyclerView.setLayoutManager(layoutManager);
+
+            // Установим адаптер
+            final NotesAdapter adapter = new NotesAdapter(data);
+            recyclerView.setAdapter(adapter);
+
+            // Добавим разделитель карточек
+            DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(),  LinearLayoutManager.VERTICAL);
+            itemDecoration.setDrawable(getResources().getDrawable(R.drawable.separator, null));
+            recyclerView.addItemDecoration(itemDecoration);
+
+            // Установим слушателя
+            adapter.SetOnItemClickListener(new NotesAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(View view, int position) {
+                    Toast.makeText(getContext(), String.format("Позиция - %d", position),
+                            Toast.LENGTH_SHORT).show();
+                    showPortCoatOfArms(position);
+                }
+            });
+        }
+        private void showPortCoatOfArms(int currentNote) {
+            //Получить менеджер фрагментов
+            FragmentManager fragmentManager = getParentFragmentManager();
+
+            // Открыть транзакцию
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            // Добавить фрагмент
+            fragmentTransaction.replace(R.id.fragment_container, ChosenNoteFragment.newInstance(currentNote));
+            fragmentTransaction.addToBackStack(null);
+            // Закрыть транзакцию
+            fragmentTransaction.commit();
+        }
+    }
 }
 
